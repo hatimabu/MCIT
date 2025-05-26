50203fe1b0110ab7e018e5f030c86f0804a7e32d
Your Name <your.email@example.com>
2025-05-25 22:19:56 -0400

    new

--- homework/homework8/main.py
+++ homework/homework8/main.py
@@ -1,7 +1,12 @@
 import my_temperature
+# Prompt user for input
 celsius_input = float(input("Enter temperature in Celsius: "))
 fahrenheit_input = float(input("Enter temperature in Fahrenheit: "))
+
+# Use functions from the temperature module
 converted_to_fahrenheit = my_temperature.celsius_to_fahrenheit(celsius_input)
 converted_to_celsius = my_temperature.fahrenheit_to_celsius(fahrenheit_input)
+
+# Print results
 print(f"{celsius_input}°C is {converted_to_fahrenheit:.2f}°F")
 print(f"{fahrenheit_input}°F is {converted_to_celsius:.2f}°C")
