e811d3a7e5667d71e04a4e1e05f2b8bf35a45636
Your Name <your.email@example.com>
2025-05-25 21:20:49 -0400

    all

--- new.py
+++
@@ -1,7 +0,0 @@
-def runningSum(nums):
-        n = len(nums)
-        result = [0] * n
-        result[0] = nums[0]
-        for i in range(1, n):
-            result[i] = result[i - 1] + nums[i]
-        return result
